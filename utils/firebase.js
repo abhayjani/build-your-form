2adf5c01327d21faabf5e992380acd129e30ab6b
abhayjani <abhayjani96@gmail.com>
2023-04-06 01:29:44 +0530

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,1 +1,22 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Replace with your Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDHk4jHuvHKgHsyLYjBf98lTMDGue8uPB4',
+  authDomain: 'build-your-form.firebaseapp.com',
+  projectId: 'build-your-form',
+  storageBucket: 'build-your-form.appspot.com',
+  messagingSenderId: '735430826170',
+  appId: '1:735430826170:web:da52769af6e6bda7608a83',
+};
+
+// Initialize the Firebase app
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+// Export Firestore instance for use in other parts of your application
+export default db;
 
