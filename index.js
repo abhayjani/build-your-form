8475738837b4f496683785e27e38b0ff86998155
abhayjani <abhayjani96@gmail.com>
2023-04-05 22:36:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname)));
 
 // Initialize Firebase Admin SDK
 const serviceAccount = require('./serviceAccountKey.json');
