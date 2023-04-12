9940c952497b038bb8b8cf2ef69ef691e37e60ac
abhayjani <abhayjani96@gmail.com>
2023-04-12 10:52:49 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,18 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage';
 import CreateFormPage from './pages/CreateFormPage/CreateFormPage';
-// Import other pages when they are created
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/create-form" component={CreateFormPage} />
-          {/* Add other routes when pages are created */}
-        </Switch>
-      </div>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/create-form" component={CreateFormPage} />
+      </Switch>
     </Router>
   );
 }
