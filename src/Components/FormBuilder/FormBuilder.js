6cc5026c587d6aeb9e167abcb304209fa04bf5b8
abhayjani <abhayjani96@gmail.com>
2023-04-12 10:42:57 +0530

    Create FormBuilder.js

---
+++ src/Components/FormBuilder/FormBuilder.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './FormBuilder.css';
+
+const FormBuilder = () => {
+  // You will implement the form builder functionality here
+  return (
+    <div className="form-builder">
+      <p>Form Builder will be implemented here.</p>
+    </div>
+  );
+};
+
+export default FormBuilder;
